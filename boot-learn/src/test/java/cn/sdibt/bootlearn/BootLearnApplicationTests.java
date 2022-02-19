1eff47cc78423f8d806e402b618324e5f3a0e909
heshutian <1442479944@qq.com>
2022-02-19 08:51:11 +0800

    新建index.html文件

---
+++ boot-learn/src/test/java/cn/sdibt/bootlearn/BootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sdibt.bootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
